eb594d9e557393649d3cdf65f31d29884dff42fc
Paul Chege <pchegenjenga@gmail.com>
2025-07-17 14:39:03 +0300

    Added total computing with delivery charge

--- basket.rb
+++ basket.rb
@@ -15,13 +15,30 @@ class Basket
     product = product_details.find { |p| p['code'] == code }
     if !product.nil?
       items << Product.new(name: product['name'], code: code, price: product['price'])
-      puts items.inspect
     else
       raise StandardError, "Product code not found"
+    end
+  end
+
+  def compute_total
+    price_sum = @items.reduce(0) { |sum, item| sum + item.price }
+    total = price_sum + delivery_charge(price_sum)
+    puts DEFAULT_CURRENCY_SYMBOL + total.to_s
+  end
+
+  def delivery_charge(total)
+    if total < 50
+      4.95
+    elsif total < 90
+      2.95
+    else
+      0.0
     end
   end
 end
 
 
 basket = Basket.new
+basket.add('B01')
 basket.add('G01')
+basket.compute_total
